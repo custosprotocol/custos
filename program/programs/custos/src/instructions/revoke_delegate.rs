dd1883e456b5eae136e68b8f715ead208fa1e0c4
0xPratik <sariapratik@gmail.com>
2023-03-06 17:10:09 +0530

    revoke delegate

--- program/programs/custos/src/instructions/revoke_delegate.rs
+++ program/programs/custos/src/instructions/revoke_delegate.rs
@@ -5,6 +5,8 @@ use anchor_lang::prelude::*;
 #[derive(Accounts)]
 pub struct RevokeDelegate<'info> {
     pub authority: Signer<'info>,
+    #[account(mut,has_one= authority,close = authority)]
+    pub delegate_account: Account<'info, DelegateAccount>,
 }
 
 pub fn handler(ctx: Context<RevokeDelegate>) -> Result<()> {
